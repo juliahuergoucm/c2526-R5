a52ac2b30f287a3bf870c2e0059e45c8a424ff3c
IvanGavaaaa <ivagar13@ucm.es>
2026-02-27 12:58:14 +0100

    Subido bien si o si

--- src/pipelines/run_extraccion.py
+++ src/pipelines/run_extraccion.py
@@ -10,7 +10,7 @@ import argparse
 import sys
 from typing import Callable, Dict, List
 
-from clima.ingest_clima_historico import ingest_clima_historico
+from src.clima.ingest_clima_historico import ingest_clima_historico
 #from src.eventos.ingest import ingest_eventos
 from src.gtfs_historico.ingest import process_and_store_gtfs_range as ingest_gtfs_historico
 #from src.alertas_oficiales_tiempo_real.ingest import ingest_alertas
